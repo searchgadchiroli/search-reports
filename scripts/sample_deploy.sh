2f44a2adfc842de79675a1f6caf39b2f8d199c7e
Vinay Venu <vinayv@thoughtworks.com>
2013-01-10 12:02:08 +0530

    Praveen, Vinay | Added a new report

---
+++ scripts/sample_deploy.sh
@@ -0,0 +1,4 @@
+#Import data into your repository
+
+BUILDOMATIC_ROOT=~/Downloads/jasperreports-server-cp-4.7.0-bin/buildomatic
+${BUILDOMATIC_ROOT}/js-import.sh --input-dir ../jasperRepository/ --update
