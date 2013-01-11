2c2313470fca3042830a29ec3bb71268d878ed15
pchandra <pchandra@thoughtworks.com>
2013-01-11 18:06:27 +0530

    praveen|adding sample export script

--- scripts/sample_deploy.sh
+++ scripts/sample_deploy.sh
@@ -1,4 +1,4 @@
 #Import data into your repository
 
-BUILDOMATIC_ROOT=~/Downloads/jasperreports-server-cp-4.7.0-bin/buildomatic
+BUILDOMATIC_ROOT=~/softwares/jasperreports-server-cp-4.7.0-bin/buildomatic
 ${BUILDOMATIC_ROOT}/js-import.sh --input-dir ../jasperRepository/ --update
---
+++ scripts/sample_export.sh
@@ -0,0 +1,4 @@
+#Export data from your repository
+
+BUILDOMATIC_ROOT=~/softwares/jasperreports-server-cp-4.7.0-bin/buildomatic
+${BUILDOMATIC_ROOT}/js-export.sh --output-dir ~/reports --uris /reports
