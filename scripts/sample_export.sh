4fae736f9627d080f27ee10e015b25fea561bded
sushmitharaos <sushmitharaos@gmail.com>
2013-01-24 15:05:03 +0530

    Sush,Arathy|1) added change set to add sample villages to program villages. 2)changed the export script to take buildomatic home as an argument.

--- scripts/sample_export.sh
+++ scripts/sample_export.sh
@@ -1,4 +1,17 @@
 #Export data from your repository
+BUILDOMATIC_ROOT=$1
 
-BUILDOMATIC_ROOT=~/softwares/jasperreports-server-cp-4.7.0-bin/buildomatic
-${BUILDOMATIC_ROOT}/js-export.sh --output-dir ~/test/reports --uris /reports
+usage() {
+	echo "Make sure that you are running the script from the 'scripts' folder. Paths are relative!"
+	echo "Usage: sample_export.sh <BUILDOMATIC_HOME>"
+}
+
+
+if [ "${BUILDOMATIC_ROOT}a" = "a" ]
+then
+	echo "Please specify BUILDOMATIC_HOME"
+	usage
+	exit 1
+fi
+
+${BUILDOMATIC_ROOT}/js-export.sh --output-dir ../jasperRepository --everything
